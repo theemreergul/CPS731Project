baa000c06e8f67668315966cffc98db65e023181
Emre <eergul@ryerson.ca>
2020-11-09 11:44:23 -0500

    SharedPreferences implemented for settings page

--- app/src/main/java/com/example/cps731project/Settings.java
+++ app/src/main/java/com/example/cps731project/Settings.java
@@ -4,19 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
     private SharedPreferences mPreferences;
-    private String sharedPrefFile =
-            "com.example.android.cps731project";
+    private String sharedPrefFile = "com.example.android.cps731project";
+    private boolean mNotifications;
+    private String mDisplayName="John Smith";
+    private final String DISPLAY_KEY = "display";
+    private final String NOTIFICATIONS_KEY="notifications";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        final TextView mNotificationsView=findViewById(R.id.txtShowNotifications);
+        final TextView mDisplayNameInput = findViewById(R.id.edtDisplayName);
+        final Switch mNotificationsSwitch=findViewById(R.id.swNotifications);
+        Button saveChanges = findViewById(R.id.btnSave);
+        mNotifications = mPreferences.getBoolean(NOTIFICATIONS_KEY, true);
+        mDisplayName=mPreferences.getString(DISPLAY_KEY,"JohnSmith");
+        mNotificationsSwitch.setChecked(mNotifications);
+        mDisplayNameInput.setText(String.format("%s", mDisplayName));
+        mNotificationsView.setText(String.format("Your notification is set to %b",mNotifications));
+        mNotificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mNotifications=isChecked;
+            }
+        });
+        saveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDisplayName= mDisplayNameInput.getText().toString();
+                mDisplayNameInput.setText(String.format("%s", mDisplayName));
+                mNotificationsView.setText(String.format("%s",mNotifications));
+                displayToast("Changes Saved");
+
+            }
+        });
+
     }
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putString(DISPLAY_KEY, mDisplayName);
+        preferencesEditor.putBoolean(NOTIFICATIONS_KEY, mNotifications);
+        preferencesEditor.apply();
     }
 }
